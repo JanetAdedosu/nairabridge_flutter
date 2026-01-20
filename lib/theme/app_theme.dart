36dcd0d7241eafe7763ea84c437438eb674a3afd
JanetAdedosu <adedosuadeola71@gmail.com>
2026-01-20 03:25:04 +0100

    Light theme configuration

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -3,5 +3,28 @@ import 'package:flutter/material.dart';
 /// Central place for all app themes
 /// This makes future redesigns very easy
 class AppTheme {
-  
+  // Light theme configuration
+  static final ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: const Color(0xFFF9F7FB),
+    primaryColor: Colors.deepPurple,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.deepPurple,
+      brightness: Brightness.light,
+    )
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      foregroundColor: Colors.black,
+    ),
+    textTheme: const TextTheme(
+      headlineLarge: TextStyle(
+        fontSize: 28,
+        fontWeight: FontWeight.bold,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 16,
+      ),
+    ),
+  );
 }
